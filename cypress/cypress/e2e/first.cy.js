49f4012c264d1d7391542541796681d73ba9e99b
enderta <endertanver@gmail.com>
2022-12-05 14:06:45 +0000

    cyp

---
+++ cypress/cypress/e2e/first.cy.js
@@ -0,0 +1,28 @@
+///<reference types="cypress"/>
+require("cypress-xpath");
+
+describe('firsttest', function () {
+    context('720p resolution', () => {
+        beforeEach(() => {
+            cy.viewport(1280, 720)
+        })
+
+        it('should be', function () {
+            Cypress.on('uncaught:exception', (err, runnable) => {
+                // returning false here prevents Cypress from
+                // failing the test
+                return false
+            })
+            cy.visit('https://www.gmibank.com/')
+            cy.get('#account-menu > .dropdown-toggle').click()
+            cy.get('#login-item > span').click()
+            cy.get('#username').type('gino.wintheiser')
+            cy.get('#password').type('%B6B*q1!TH')
+            cy.xpath("//button[@type='submit']").click()
+            cy.xpath("(//*[@class='dropdown nav-item']//a//span)[5]").should('have.text','Joe King')
+
+        })
+
+
+    });
+});
