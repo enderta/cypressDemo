d5847e3d791e680a106d0dda67e42689237c1b59
enderta <endertanver@gmail.com>
2024-11-09 23:57:37 +0000

    azure

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/integration/**/*.cy.{js,jsx,ts,tsx}", // Update to match your test file path
-    
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Update to match your test file path
+
   },
 });
