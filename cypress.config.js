3b1cb0346b17529c7c754faee4f30dbfdc7e0a5d
enderta <endertanver@gmail.com>
2024-11-09 23:55:26 +0000

    azure

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Update to match your test file path
-    baseUrl: "http://localhost:3000", // Ensure this matches your server URL
+    
   },
 });
